f5993388a737dc2ef86f5d66b582e776b01c0858
David <0david0strobach0@gmail.com>
2025-09-24 18:52:13 +0200

    Fix: Disable Sanity CDN to resolve upcoming exhibition updates not reflecting on live site

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -4,5 +4,5 @@ export const sanityClient = createClient({
   projectId: 'yfd7xjeg',
   dataset: 'production',
   apiVersion: '2025-08-01',
-  useCdn: true,
+  useCdn: false, // Disable CDN to get immediate updates
 })
