9ec6a6bae01056faca7464e40e80380da6e30b30
David <0david0strobach0@gmail.com>
2025-10-01 19:44:09 +0200

    Add appId to sanity.cli.ts to avoid deployment prompts

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,6 +5,9 @@ export default defineCliConfig({
     projectId: 'yfd7xjeg',
     dataset: 'production'
   },
+  deployment: {
+    appId: 'y37xxnbv8at5ph1jew0hci7f',
+  },
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
